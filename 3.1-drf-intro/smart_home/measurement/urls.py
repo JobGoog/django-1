dedeb32fba17381fd44a04f796794cabdf3865b3
JobGoog <roro425052@gmail.com>
2023-05-04 14:49:47 +0300

    done

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from django.contrib import admin
+
+from measurement.views import CreateAPIView, ListCreateAPIView, ListView, RetrieveUpdateAPIView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('admin/', admin.site.urls),
+    path('sensors/', CreateAPIView.as_view()),
+    path('sensors/<pk>/', RetrieveUpdateAPIView.as_view()),
+    path('list/', ListView.as_view()),
+    path('measurements/', ListCreateAPIView.as_view()),
 ]
